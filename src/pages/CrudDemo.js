cd8b371dbc407b0169c3504201dd8d629ac65e60
dahibi ghita <dahibighita@gmail.com>
2023-08-04 09:45:24 +0200

    commit

--- src/pages/CrudDemo.js
+++ src/pages/CrudDemo.js
@@ -88,10 +88,7 @@ const CrudDemo = () => {
   const header = (
     <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
       <h5 className="m-0">Gestion des Offres d'emploie</h5>
-      <span className="block mt-2 md:mt-0 p-input-icon-left">
-        <i className="pi pi-search" />
-        <InputText type="search" placeholder="Chercher..." />
-      </span>
+     
     </div>
   );
 
